a2f9cba78afa5d40680d61673723a4fc5de1f010
Akinmyde <akinremiolumide96@gmail.com>
2018-12-15 16:31:07 +0100

    add informatiom message for success and refactor signup.js

--- UI/js/signup.js
+++ UI/js/signup.js
@@ -5,33 +5,25 @@ const email = document.getElementById('email')
       error = document.getElementsByClassName('error')[0];
 
 document.getElementById('signup').addEventListener('click', (e) => {
+  e.preventDefault()
+  error.style.display = 'block'
   if (email.value === '') {
-    return (
-      e.preventDefault(),
-      error.innerHTML = 'Email is required'
-    )
-  } if (username.value === '') {
-    return (
-      e.preventDefault(),
-      error.innerHTML = 'Username is required'
-    )
-  } if (password.value === '') {
-    return (
-      e.preventDefault(),
-      error.innerHTML = 'Password is required'
-    )
-  } if (confirmPassword.value === '') {
-    return (
-      e.preventDefault(),
-      error.innerHTML = 'Please confirm password'
-    )
-  } if (confirmPassword.value !== password.value) {
-    return (
-      e.preventDefault(),
-      error.innerHTML = 'Password must be the same'
-    )
-  } return (
-    e.preventDefault(),
-    error.innerHTML = 'Accout was created, check your email for the next step'
+    return error.innerHTML = 'Email is required';
+  } 
+  if (username.value === '') {
+    return error.innerHTML = 'Username is required';
+  } 
+  if (password.value === '') {
+    return error.innerHTML = 'Password is required';
+  } 
+  if (confirmPassword.value === '') {
+    return error.innerHTML = 'Please confirm password';
+  } 
+  if (confirmPassword.value !== password.value) {
+    return  error.innerHTML = 'Password must be the same';
+  } 
+  return (
+    error.innerHTML = 'Accout was created, check your email for the next step.',
+    error.style.color = 'green'
   )
 })
