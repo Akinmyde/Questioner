1109a483f005bc040ce0fbd30670024f25753e05
Akinmyde <akinremiolumide96@gmail.com>
2018-12-15 16:22:45 +0100

    show success message on sign-in

--- UI/js/signin.js
+++ UI/js/signin.js
@@ -4,23 +4,25 @@ const error = document.getElementsByClassName('error')[0];
 error.style.display = 'none';
 
 document.getElementById('login').addEventListener('click', (e) => {
+  e.preventDefault()
   if (username.value === '') {
     return (
-      e.preventDefault(),
       error.innerHTML = 'Username is empty',
       error.style.display = 'block'
     )
   } if (password.value === '') {
     return (
-      e.preventDefault(),
       error.innerHTML = 'Password is empty',
       error.style.display = 'block'
     )
   } if(username.value.toLowerCase() !== 'andela' && password.value !== 'testing') {
     return (
-      e.preventDefault(),
       error.innerHTML = 'Invalid Username and/or Password',
       error.style.display = 'block'
     )
-  } 
+  } return (
+    error.innerHTML = 'Login was Successful',
+    error.style.display = 'block',
+    error.style.color = 'green'
+  )
 });
