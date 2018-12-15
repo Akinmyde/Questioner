aac05f876b425f3476071b951b1cec803e83fb14
Akinmyde <akinremiolumide96@gmail.com>
2018-12-16 00:20:48 +0100

    added navigation link

---
+++ UI/js/index.js
@@ -0,0 +1,7 @@
+const header = document.getElementsByClassName('header')[0];
+
+// if (header.className !== 'header') {
+//   header.className += ' responsive';
+// } else {
+//   header.className = 'header';
+// }
