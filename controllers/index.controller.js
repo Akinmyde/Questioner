61c66276f2018ec9697a176f59934dd42682cb45
Akinmyde <akinremiolumide96@gmail.com>
2018-12-21 09:24:47 +0100

    endpoint to get all meetup

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -20,4 +20,17 @@ const createMeetup = (req, res) => {
   return res.status(400).send({ error: 'meetup not created' });
 };
 
-module.exports = { createMeetup };
+const getAllMeetup = (req, res) => {
+  if (db.length > 0) {
+    return res.status(200).send({
+      status: 200,
+      data: db,
+    });
+  }
+  return res.status(404).send({
+    status: 404,
+    error: 'Not found',
+  });
+};
+
+module.exports = { createMeetup, getAllMeetup };
