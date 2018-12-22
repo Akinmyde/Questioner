e996a1070f3141029d90459599cee7a2e83e1645
Akinmyde <akinremiolumide96@gmail.com>
2018-12-22 13:37:18 +0100

    added array data structure to store questions

--- db/meetups.js
+++ db/meetups.js
@@ -19,4 +19,25 @@ const meetup = [
   },
 ];
 
-module.exports = meetup;
+const question = [
+  {
+    id: 1,
+    createdOn: Date,
+    createdBy: 21, // represents the user asking the question
+    meetup: 11, // represents the meetup the question is for
+    title: 'What Do I Need To Do To Succeed In The Bootcamp',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quod accusamus repudiandae impedit quasi quidem assumenda consectetur ab, beatae sit ea provident obcaecati, maxime neque ipsa ut sunt consequatur. Tenetur!',
+    vote: 0,
+  },
+  {
+    id: 2,
+    createdOn: Date,
+    createdBy: 20, // represents the user asking the question
+    meetup: 15, // represents the meetup the question is for
+    title: 'Please I Need To Know More About The Bootcamp',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quod accusamus repudiandae impedit quasi quidem assumenda consectetur ab, beatae sit ea provident obcaecati, maxime neque ipsa ut sunt consequatur. Tenetur!',
+    vote: 0,
+  },
+];
+
+module.exports = { meetup, question };
