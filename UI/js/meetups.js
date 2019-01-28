4df3adea76a096e24667e918296a4f133de2f4a3
Akinmyde <akinremiolumide96@gmail.com>
2019-01-28 16:18:47 +0100

    [Feature #163512613] implement Fetch Api for all meetups page

---
+++ UI/js/meetups.js
@@ -0,0 +1,32 @@
+fetch('https://akinmyde-questioner.herokuapp.com/api/v1/meetups')
+  .then(resp => resp.json())
+  .then((results) => {
+    const { data } = results;
+    const container = document.getElementsByClassName('meetup-card')[0];
+    data.forEach((meetup) => {
+      const {
+        id,
+        topic,
+        happeningon,
+        location,
+        images,
+      } = meetup;
+      // meetup-single.html
+      const card = `<a class="meetup-link" id="${id}" href="#">
+    <div>
+      <img class="image" src="${images[0]}" alt="image">
+      <h4><a id=${id} class="meetup-link" href="#">${topic}</a></h4>
+      <h6>@${location}</h6>
+      <span class="text-holder">
+        <ul class="details">
+          <li>${new Date(happeningon).toDateString()}</li>
+        </ul>
+      </span>
+    </div>
+  </a>`;
+      container.insertAdjacentHTML('afterbegin', card);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
