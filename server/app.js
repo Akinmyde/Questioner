dbc7439192d38367ca516c8a67475a885a010a9d
Akinmyde <akinremiolumide96@gmail.com>
2019-01-28 09:51:08 +0100

    [Bug #163511298] fix cors issues

--- server/app.js
+++ server/app.js
@@ -2,18 +2,24 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import chalk from 'chalk';
 import morgan from 'morgan';
+import cors from 'cors';
+
 import router from './routes/index';
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/api/v1', router);
+
 app.all('*', (req, res) => {
   res.status(404).send({ status: 404, error: 'Sorry, the page you tried cannot be found' });
 });
+
 app.listen(port, () => {
   console.log(`listening on port ${chalk.blue(port)}`);
 });
