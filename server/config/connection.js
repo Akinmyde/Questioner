6fe7c0cdbe3efc1d6ce43684ceed3ee4f26d7ce5
Akinremi Olumide <akinremiolumide96@gmail.com>
2019-01-30 19:39:53 +0100

    [Chore #163581250] refactor server code

--- server/config/connection.js
+++ server/config/connection.js
@@ -6,7 +6,7 @@ dotenv.config();
 const config = {
   development: process.env.DATABASE_DEVELOPMENT,
   test: process.env.DATABASE_TEST,
-  production: process.env.DATABASE_URL
+  production: process.env.DATABASE_URL,
 };
 
 const env = process.env.NODE_ENV || 'development';
