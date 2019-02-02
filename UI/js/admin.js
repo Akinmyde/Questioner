a191e915059f96db8b251516347209e845f1c687
Akinmyde <akinremiolumide96@gmail.com>
2019-02-02 16:39:43 +0100

    [Feature #163623504] Implement Fetch Api for admin dashboard and integrate admin functionalities

--- UI/js/admin.js
+++ UI/js/admin.js
@@ -1,76 +1,66 @@
-const image = document.getElementById('image');
+/* eslint-disable no-useless-return */
 const error = document.getElementsByClassName('error')[0];
-const topic = document.getElementById('topic');
+const meetupTopic = document.getElementById('topic');
 const eventLocation = document.getElementById('location');
 const date = document.getElementById('date');
-const createView = document.getElementById('create');
-const dashboard = document.getElementById('dashboard');
-const linkMeetups = document.getElementById('linkMeetups');
-const meetupsView = document.getElementById('meetups');
-const classActive = document.getElementsByClassName('active');
-const deleteMeetup = document.getElementsByClassName('delete');
+const loader = document.getElementById('overlay');
+const alert = document.getElementsByClassName('alert')[0];
+const token = localStorage.getItem('token');
+const totalmeetups = document.getElementById('totalmeetups');
 
-meetupsView.style.display = 'none';
-dashboard.addEventListener('click', (e) => {
-  e.preventDefault();
-  for (let i = 0; i < classActive.length; i++) {
-    classActive[i].className = ''
-  }
-  dashboard.className = 'active';
-  meetupsView.style.display = 'none';
-  createView.style.display = 'block';
-});
-
-linkMeetups.addEventListener('click', (e) => {
-  e.preventDefault();
-  for (let i = 0; i < classActive.length; i++) {
-    classActive[i].className = '';
-  }
-  linkMeetups.className = 'active';
-  meetupsView.style.display = 'flex';
-  createView.style.display = 'none';
-})
-
-for (let i = 0; i < deleteMeetup.length;  i++) {
-  deleteMeetup[i].addEventListener('click', (e) => {
-    e.preventDefault();
-    deleteMeetup[i].parentNode.parentNode.parentNode.parentNode.style.display = 'none';
-  })
-}
-
-document.getElementById('upload').addEventListener('change', () => {
-  image.style.display = 'none';
-  error.style.display = 'none';
-  const file = event.target.files[0];
-  if (file) {
-    const fileType = file.type.split('/')[1];
-    if (fileType === 'jpeg' || fileType === 'jpg' || fileType === 'png') {
-      if (file.size < 4000000) {
-        return (
-          image.style.display = 'block',
-          image.src = URL.createObjectURL(file)
-        )
-      } return (
-        error.style.display = 'block',
-        error.innerHTML = 'File size must not exceed 4mb'
-      )
-    } return (
-      error.style.display = 'block',
-      error.innerHTML = 'File type must be jpeg or png'
-    )
-  }
-});
-
+loader.style.display = 'block';
+fetch('https://akinmyde-questioner.herokuapp.com/api/v1/meetups')
+  .then(resp => resp.json())
+  .then((results) => {
+    loader.style.display = 'none';
+    const { data } = results;
+    totalmeetups.innerHTML = data.length;
+  });
+// loader.style.display = 'none';
+error.style.display = 'block';
 document.getElementById('btnmeetup').addEventListener('click', (e) => {
-  error.style.display = 'none'
   e.preventDefault();
-  if (topic.value === '' || eventLocation.value === '' || date.value === '') {
-    return (
-      error.style.display = 'block',
-      error.innerHTML = 'All fields are required'
-    )
+  if (meetupTopic.value === '' || eventLocation.value === '' || date.value === '') {
+    error.innerHTML = 'All fields are required';
+    alert.style.display = 'block';
+    return;
   }
-  error.style.color = 'green',
-  error.innerHTML = 'Event was added successfully',
-  error.style.display = 'block'
-})
+  if (meetupTopic.value === '') {
+    error.innerHTML = 'meetup topic is required';
+    alert.style.display = 'block';
+    return;
+  }
+  if (eventLocation.value === '') {
+    error.innerHTML = 'location is required';
+    alert.style.display = 'block';
+    return;
+  }
+  if (date.value === '') {
+    error.innerHTML = 'date is required';
+    alert.style.display = 'block';
+    return;
+  }
+  loader.style.display = 'block';
+  const url = 'https://akinmyde-questioner.herokuapp.com/api/v1/meetups';
+  const newMeetup = {
+    topic: meetupTopic.value,
+    location: eventLocation.value,
+    happeningOn: date.value,
+  };
+  const fetchData = {
+    method: 'POST',
+    body: JSON.stringify(newMeetup),
+    headers: { 'Content-Type': 'application/json', token },
+  };
+  fetch(url, fetchData)
+    .then(res => res.json())
+    .then((result) => {
+      const number = parseInt(totalmeetups.innerHTML, 10);
+      totalmeetups.innerHTML = number + 1;
+      loader.style.display = 'none';
+      error.innerHTML = result.message;
+      alert.style.display = 'block';
+      alert.style.background = 'rgb(64, 141, 64)';
+    })
+    .catch(err => console.log(err));
+});
--- UI/js/meetups.js
+++ UI/js/meetups.js
@@ -1,6 +1,13 @@
 const container = document.getElementsByClassName('meetup-card')[0];
 const link = document.getElementsByTagName('a');
 const loader = document.getElementById('overlay');
+const user = JSON.parse(localStorage.getItem('user'));
+let isAdmin;
+if (user) {
+  isAdmin = user.isadmin;
+} else {
+  isAdmin = false;
+}
 
 fetch('https://akinmyde-questioner.herokuapp.com/api/v1/meetups')
   .then(resp => resp.json())
@@ -9,25 +16,36 @@ fetch('https://akinmyde-questioner.herokuapp.com/api/v1/meetups')
     const { data } = results;
     sessionStorage.setItem('meetupObj', JSON.stringify(data));
     data.forEach((meetup) => {
-      const {
-        id,
-        topic,
-        happeningon,
-        location,
-        images,
-      } = meetup;
-      const card = `<a class="meetup-link" id="${id}" href="meetup-single.html">
-    <div>
-      <img class="image" src="${images[0]}" alt="image">
-      <h4><a id=${id} class="meetup-link" href="meetup-single.html">${topic}</a></h4>
-      <h6>@${location}</h6>
-      <span class="text-holder">
-        <ul class="details">
-          <li>${new Date(happeningon).toDateString()}</li>
-        </ul>
-      </span>
-    </div>
-  </a>`;
+      let card;
+      if (!isAdmin) {
+        card = `<a class="meetup-link" id="${meetup.id}" href="meetup-single.html">
+        <div>
+          <img class="image" src="${meetup.images[0]}" alt="image">
+          <h4><a id=${meetup.id} class="meetup-link" href="meetup-single.html">${meetup.topic}</a></h4>
+          <h6>@${meetup.location}</h6>
+          <span class="text-holder">
+            <ul class="details">
+              <li>${new Date(meetup.happeningon).toDateString()}</li>
+            </ul>
+          </span>
+        </div>
+      </a>`;
+      } else {
+        card = `<a class="meetup-link" id="${meetup.id}" href="meetup-single.html">
+        <div>
+          <img class="image" src="${meetup.images[0]}" alt="image">
+          <h4><a id=${meetup.id} class="meetup-link" href="meetup-single.html">${meetup.topic}</a></h4>
+          <h6>@${meetup.location}</h6>
+          <span class="text-holder">
+            <ul class="details">
+              <li>${new Date(meetup.happeningon).toDateString()}</li>
+              <li title="delete"><a href="" id="${meetup.id}" class="delete"><i class="fas fa-trash"></i></a></li>
+            </ul>
+          </span>
+        </div>
+      </a>`;
+      }
+
       container.insertAdjacentHTML('afterbegin', card);
     });
 
@@ -38,6 +56,30 @@ fetch('https://akinmyde-questioner.herokuapp.com/api/v1/meetups')
           e.preventDefault();
           sessionStorage.setItem('meetupid', parseInt(link[i].id, 10));
           window.location.href = 'meetup-single.html';
+        });
+      }
+    }
+    // Delete meetup
+    if (isAdmin) {
+      const deleteMeetup = document.getElementsByClassName('delete');
+      for (let i = 0; i < deleteMeetup.length; i += 1) {
+        deleteMeetup[i].addEventListener('click', (e) => {
+          e.preventDefault();
+          const meetupId = deleteMeetup[i].id;
+          const url = `https://akinmyde-questioner.herokuapp.com/api/v1/meetups/${meetupId}`;
+          const token = localStorage.getItem('token');
+          const fetchData = {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json', token },
+          };
+          loader.style.display = 'block';
+          fetch(url, fetchData)
+            .then(res => res.json())
+            .then((resp) => {
+              console.log(resp);
+              deleteMeetup[i].parentNode.parentNode.parentNode.parentNode.style.display = 'none';
+              loader.style.display = 'none';
+            });
         });
       }
     }
